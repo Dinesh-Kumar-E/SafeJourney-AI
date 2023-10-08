b6a15a2c76f6662d95fc7cf88f36cc46386a0d85
Dinesh Kumar E <dinesh_kumar_e@outlook.com>
2023-10-08 18:36:17 +0530

    Updated Get_weights.py file for error checking

--- modules/Get_weights.py
+++ modules/Get_weights.py
@@ -1,27 +1,30 @@
 import os
 import requests
 
-
 def get_url(model):
     data = {
-          "shape_predictor_68_face_landmarks.dat" : r"https://files.catbox.moe/84pk4m.dat"
-     }
+        "shape_predictor_68_face_landmarks.dat": r"https://files.catbox.moe/84pk4m.dat"
+    }
     return data[model]
 
-def name(model_name,default = r"resources"):
+def name(model_name, default=r"resources"):
+    # Check if the 'default' directory exists, create it if not
+    if not os.path.exists(default):
+        os.makedirs(default)
+
     model_file_path = os.path.join(default, model_name)
-    if (model_name) in os.listdir(default):
-        print("Model already exists in : ", model_file_path)
+    if model_name in os.listdir(default):
+        print("Model already exists in:", model_file_path)
         return model_file_path
     else:
         print("Downloading model...")
         response = requests.get(get_url(model_name))
         if response.status_code == 200:
             file_name = os.path.join(default, model_name)
-            
+
             with open(file_name, 'wb') as file:
                 file.write(response.content)
-                
+
             return file_name
         else:
             return None
